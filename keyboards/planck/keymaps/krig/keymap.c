faa114d0d0096b3f0c37fc0fe2eaacd7ec87e3b1
Kristoffer Grönlund <krig@koru.se>
2021-07-09 22:56:19 +0200

    Align planck layout with preonic layout (and vice versa)

--- keyboards/planck/keymaps/krig/keymap.c
+++ keyboards/planck/keymaps/krig/keymap.c
@@ -4,6 +4,21 @@
 #endif
 #include "eeprom.h"
 
+
+enum planck_keycodes {
+  RGB_SLD = EZ_SAFE_RANGE,
+};
+
+enum planck_layers {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _GAME,
+  _NUMERIC
+};
+
+
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
 #define KC_MAC_COPY LGUI(KC_C)
@@ -18,67 +33,52 @@
 #define NO_PIPE_ALT KC_GRAVE
 #define NO_BSLS_ALT KC_EQUAL
 
-enum planck_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
-};
-
-enum planck_layers {
-  _BASE,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
-  _GAMING,
-  _NUMERIC
-};
-
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
-      KC_TAB            ,      KC_Q ,    KC_W ,    KC_E ,  KC_R ,     KC_T ,  KC_Y ,   KC_U ,     KC_I  ,    KC_O ,      KC_P , KC_BSPACE,
-      LCTL_T(KC_ESCAPE) ,      KC_A ,    KC_S ,    KC_D ,  KC_F ,     KC_G ,  KC_H ,   KC_J ,     KC_K  ,    KC_L , KC_SCOLON ,  KC_ENTER,
-      KC_LSHIFT         ,      KC_Z ,    KC_X ,    KC_C ,  KC_V ,     KC_B ,  KC_N ,   KC_M , KC_COMMA  ,  KC_DOT ,  KC_SLASH ,  KC_QUOTE,
-      MO(_NUMERIC)      , KC_ESCAPE , KC_LALT , KC_LGUI , LOWER , KC_SPACE , KC_NO , RAISE  ,   KC_LEFT , KC_DOWN ,     KC_UP ,  KC_RIGHT
+      KC_TAB,         KC_Q,            KC_W,       KC_E,    KC_R,       KC_T,     KC_Y,    KC_U,       KC_I,     KC_O,       KC_P,       KC_BSPACE,
+      LCTL_T(KC_ESC), KC_A,            KC_S,       KC_D,    KC_F,       KC_G,     KC_H,    KC_J,       KC_K,     KC_L,       KC_SCOLON,  KC_ENTER,
+      KC_LSHIFT,      KC_Z,            KC_X,       KC_C,    KC_V,       KC_B,     KC_N,    KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   KC_QUOTE,
+      MO(_NUMERIC),   LCTL(KC_LSHIFT), KC_LALT,    KC_LGUI, MO(_LOWER), KC_SPACE, KC_NO,   MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_TILD ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,KC_CIRC ,KC_AMPR  ,KC_ASTR ,RALT(KC_P) ,RALT(KC_Q) ,RALT(KC_W),
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_LPRN ,KC_RPRN ,KC_PIPE ,KC_PLUS ,KC_MINUS ,KC_UNDS ,KC_EQUAL   ,KC_TRNS    ,KC_TRNS,
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS  ,KC_TRNS ,KC_TRNS    ,KC_BSLASH  ,KC_GRAVE,
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_NO   ,KC_TRNS  ,KC_HOME ,KC_PGDOWN  ,KC_PGUP    ,KC_END
+      KC_TILD,        KC_EXLM,         KC_AT,      KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC, KC_AMPR,    KC_ASTR,  RALT(KC_P), RALT(KC_Q), KC_TRNS,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_LPRN, KC_RPRN,    KC_PIPE,  KC_PLUS, KC_MINUS,   KC_UNDS,  KC_EQUAL,   RALT(KC_W), KC_TRNS,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_BSLASH,  KC_GRAVE,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_NO,   KC_TRNS,    KC_HOME,  KC_PGDOWN,  KC_PGUP,    KC_END
   ),
 
   [_RAISE] = LAYOUT_planck_grid(
-      KC_TRNS ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7        ,KC_8        ,KC_9    ,KC_0    ,KC_DELETE,
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_LCBR ,KC_RCBR ,KC_TRNS ,KC_TRNS ,KC_LBRACKET ,KC_RBRACKET ,KC_TRNS ,KC_TRNS ,KC_INSERT,
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS     ,KC_TRNS     ,KC_TRNS ,KC_TRNS ,KC_TRNS,
-      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_NO   ,KC_TRNS     ,KC_TRNS     ,KC_TRNS ,KC_TRNS ,KC_TRNS
+      KC_TRNS,        KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,     KC_9,       KC_0,       KC_DELETE,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_LCBR, KC_RCBR,    KC_LABK,  KC_RABK, KC_LBRC,    KC_RBRC,  KC_QUOT,    KC_DQUO,    KC_INSERT,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_NO,   KC_TRNS,    KC_HOME,  KC_PGDOWN,  KC_PGUP,    KC_END
   ),
 
   [_ADJUST] = LAYOUT_planck_grid(
-      KC_SCROLLLOCK ,KC_F1   ,KC_F2      ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_F7   ,KC_F8   ,KC_F9         ,KC_F10            ,KC_F11,
-      KC_CAPSLOCK   ,KC_F12  ,KC_PSCREEN ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,RGB_HUD ,RGB_HUI ,KC_AUDIO_MUTE ,KC_AUDIO_VOL_DOWN ,KC_AUDIO_VOL_UP,
-      KC_TRNS       ,KC_TRNS ,KC_TRNS    ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,RGB_VAD ,RGB_VAI ,RGB_TOG       ,RGB_MOD           ,RGB_SLD,
-      KC_TRNS       ,KC_TRNS ,KC_TRNS    ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_NO   ,KC_TRNS ,KC_TRNS ,MU_TOG        ,MU_MOD            ,AU_TOG
+      KC_SCROLLLOCK,  KC_F1,           KC_F2,      KC_F3,   KC_F4,      KC_F5,    KC_F6,   KC_F7,      KC_F8,    KC_F9,      KC_F10,     KC_F11,
+      KC_CAPSLOCK,    KC_F12,          KC_PSCREEN, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, RGB_HUD,    RGB_HUI,  KC_MUTE,    KC_VOLD,    KC_VOLU,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, RGB_VAD,    RGB_VAI,  RGB_TOG,    RGB_MOD,    RGB_SLD,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_NO,   KC_TRNS,    KC_TRNS,  MU_TOG,     MU_MOD,     AU_TOG
   ),
 
-  [_GAMING] = LAYOUT_planck_grid(
-      KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS,
-      KC_LCTRL ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS,
-      KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS,
-      KC_TRNS  ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_NO   ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS
+  [_GAME] = LAYOUT_planck_grid(
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
+      KC_LCTL,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_NO,   KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS
   ),
 
   [_NUMERIC] = LAYOUT_planck_grid(
-      TG(_GAMING) ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS        ,KC_TRNS ,KC_TRNS ,KC_1   ,KC_2 ,KC_3,
-      KC_TRNS     ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS        ,KC_TRNS ,KC_TRNS ,KC_4   ,KC_5 ,KC_6,
-      KC_TRNS     ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS        ,KC_TRNS ,KC_TRNS ,KC_7   ,KC_8 ,KC_9,
-      KC_TRNS     ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,         KC_NO ,KC_TRNS ,KC_TRNS ,KC_DOT ,KC_0 ,KC_COMMA
+      TG(_GAME),      KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_1,       KC_2,       KC_3,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_4,       KC_5,       KC_6,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,  KC_7,       KC_8,       KC_9,
+      KC_TRNS,        KC_TRNS,         KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,  KC_NO,   KC_TRNS,    KC_TRNS,  KC_DOT,     KC_0,       KC_COMMA
   ),
 };
 
-extern bool g_suspend_state;
+extern bool         g_suspend_state;
 extern rgb_config_t rgb_matrix_config;
 
 
@@ -92,7 +92,7 @@ void keyboard_post_init_user(void) {
 }
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-    [_GAMING] = { {0,0,0}, {0,0,0}, {WASD_COLOR}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
+    [_GAME] = { {0,0,0}, {0,0,0}, {WASD_COLOR}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
             {0,0,0}, {WASD_COLOR}, {WASD_COLOR}, {WASD_COLOR}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
             {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0},
             {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
@@ -103,14 +103,14 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
 };
 
 void set_layer_color(void) {
-  const bool gaming = layer_state_is(_GAMING);
+  const bool gaming = layer_state_is(_GAME);
   const bool numeric = layer_state_is(_NUMERIC);
   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
       uint8_t r = 0, g = 0, b = 0;
       if (gaming) {
-          r = ledmap[_GAMING][i][0];
-          g = ledmap[_GAMING][i][1];
-          b = ledmap[_GAMING][i][2];
+          r = ledmap[_GAME][i][0];
+          g = ledmap[_GAME][i][1];
+          b = ledmap[_GAME][i][2];
       }
       if (numeric && (ledmap[_NUMERIC][i][0] || ledmap[_NUMERIC][i][1] || ledmap[_NUMERIC][i][2])) {
           r = ledmap[_NUMERIC][i][0];
@@ -126,7 +126,7 @@ void rgb_matrix_indicators_user(void) {
       return;
   }
 
-  if (layer_state_is(_GAMING) || layer_state_is(_NUMERIC))
+  if (layer_state_is(_GAME) || layer_state_is(_NUMERIC))
     set_layer_color();
   else if (layer_state_is(_ADJUST))
     rgb_matrix_set_color_all(0xb7, 0x15, 0x40);
@@ -208,8 +208,8 @@ void matrix_scan_user(void) {
 
 bool music_mask_user(uint16_t keycode) {
     switch (keycode) {
-    case RAISE:
-    case LOWER:
+    case MO(_RAISE):
+    case MO(_LOWER):
         return false;
     default:
         return true;
