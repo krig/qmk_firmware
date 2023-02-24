49a727ffd4bb706dc14dbbdfbc9454dd86bfa62c
Kristoffer Grönlund <krig@koru.se>
2023-02-24 20:25:27 +0100

    Update my keymaps to the latest commit

--- keyboards/planck/keymaps/krig/keymap.c
+++ keyboards/planck/keymaps/krig/keymap.c
@@ -7,6 +7,7 @@
 
 enum planck_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
+  DBLCOLN // ::
 };
 
 enum planck_layers {
@@ -37,30 +38,30 @@ enum planck_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
       KC_TAB,         KC_Q,            KC_W,       KC_E,    KC_R,       KC_T,     KC_Y,    KC_U,       KC_I,     KC_O,       KC_P,       KC_BSPC,
-      LCTL_T(KC_ESC), KC_A,            KC_S,       KC_D,    KC_F,       KC_G,     KC_H,    KC_J,       KC_K,     KC_L,       KC_SCLN,  KC_ENTER,
-      KC_LSFT,      KC_Z,            KC_X,       KC_C,    KC_V,       KC_B,     KC_N,    KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   KC_QUOTE,
-      MO(_NUMERIC),   LCTL(KC_LSFT), KC_LALT,    KC_LGUI, MO(_LOWER), KC_SPACE, KC_NO,   MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
+      LCTL_T(KC_ESC), KC_A,            KC_S,       KC_D,    KC_F,       KC_G,     KC_H,    KC_J,       KC_K,     KC_L,       KC_SCLN,    KC_ENTER,
+      KC_LSFT,        KC_Z,            KC_X,       KC_C,    KC_V,       KC_B,     KC_N,    KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   RSFT_T(KC_QUOTE),
+      MO(_NUMERIC),   LCTL(KC_LSFT),   KC_LALT,    KC_LGUI, MO(_LOWER), KC_SPACE, KC_NO,   MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_TILD,        KC_EXLM,         KC_AT,      KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC, KC_AMPR,    KC_ASTR,  RALT(KC_P), RALT(KC_Q), _______,
-      _______,        _______,         _______,    KC_LPRN, KC_RPRN,    KC_PIPE,  KC_PLUS, KC_MINUS,   KC_UNDS,  KC_EQUAL,   RALT(KC_W), _______,
-      _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    _______,  _______,    KC_BSLS,  KC_GRAVE,
-      _______,        _______,         _______,    _______, _______,    _______,  KC_NO,   _______,    KC_HOME,  KC_PGDN,  KC_PGUP,    KC_END
+       KC_GRV,        KC_EXLM,         KC_AT,      KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC, KC_AMPR,    KC_ASTR,  RALT(KC_P), RALT(KC_Q), _______,
+      _______,        _______,         _______,    KC_LPRN, KC_RPRN,    KC_PIPE,  KC_PLUS, KC_MINS,    KC_UNDS,      KC_EQL, RALT(KC_W), _______,
+      _______,        _______,         _______,    _______, _______,    _______,  _______, DBLCOLN,    _______,     _______,    KC_BSLS,  KC_GRV,
+      _______,        _______,         _______,    _______, _______,    _______,    KC_NO, _______,    KC_HOME,     KC_PGDN,    KC_PGUP,  KC_END
   ),
 
   [_RAISE] = LAYOUT_planck_grid(
-      KC_ESC,         KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,     KC_9,       KC_0,       KC_DELETE,
-      _______,        _______,         _______,    KC_LCBR, KC_RCBR,    KC_LABK,  KC_RABK, KC_LBRC,    KC_RBRC,  KC_QUOT,    KC_DQUO,    KC_INSERT,
+      KC_TILD,         KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,     KC_9,       KC_0,       KC_DEL,
+      _______,        _______,         _______,    KC_LCBR, KC_RCBR,    KC_LABK,  KC_RABK, KC_LBRC,    KC_RBRC,  KC_QUOT,    KC_DQUO,    KC_INS,
       _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    _______,  _______,    _______,    _______,
-      _______,        _______,         _______,    _______, _______,    _______,  KC_NO,   _______,    KC_HOME,  KC_PGDN,  KC_PGUP,    KC_END
+      _______,        _______,         _______,    _______, _______,    _______,  KC_NO,   _______,    KC_HOME,  KC_PGDN,    KC_PGUP,    KC_END
   ),
 
   [_ADJUST] = LAYOUT_planck_grid(
-      KC_SCRL,  KC_F1,           KC_F2,      KC_F3,   KC_F4,      KC_F5,    KC_F6,   KC_F7,      KC_F8,    KC_F9,      KC_F10,     KC_F11,
-      KC_CAPS,    KC_F12,          KC_PSCR, _______, _______,    _______,  _______, RGB_HUD,    RGB_HUI,  KC_MUTE,    KC_VOLD,    KC_VOLU,
-      _______,        _______,         _______,    _______, _______,    _______,  _______, RGB_VAD,    RGB_VAI,  RGB_TOG,    RGB_MOD,    RGB_SLD,
-      _______,        _______,         _______,    _______, _______,    _______,  KC_NO,   _______,    _______,  MU_TOG,     MU_MOD,     AU_TOG
+       KC_ESC,    KC_F1,    KC_F2,   KC_F3,   KC_F4,      KC_F5,    KC_F6,   KC_F7,      KC_F8,    KC_F9,     KC_F10,     KC_F11,
+      KC_CAPS,   KC_F12,  KC_PSCR, _______, _______,    _______,  _______, RGB_HUD,    RGB_HUI,  KC_MUTE,    KC_VOLD,    KC_VOLU,
+      _______,  _______,  _______, _______, _______,    _______,  _______, RGB_VAD,    RGB_VAI,  RGB_TOG,    RGB_MOD,    RGB_SLD,
+      _______,  _______,  _______, _______, _______,    _______,  KC_NO,   _______,    _______,   MU_TOG,     MU_MOD,     AU_TOG
   ),
 
   [_GAME] = LAYOUT_planck_grid(
@@ -140,6 +141,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(1);
       }
       return false;
+    case DBLCOLN:
+        if (record->event.pressed) {
+            SEND_STRING("::");
+        }
+        return false;
   }
   return true;
 }
--- keyboards/preonic/keymaps/krig/keymap.c
+++ keyboards/preonic/keymaps/krig/keymap.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_KNIGHT
+#define RGBLIGHT_DEFAULT_HUE 222 // rose
+
+
+enum custom_keycodes {
+    DBLCOLN = SAFE_RANGE // ::
+};
+
 
 enum preonic_layers {
     _BASE,
@@ -11,28 +19,24 @@ enum preonic_layers {
 };
 
 
-#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_KNIGHT
-#define RGBLIGHT_DEFAULT_HUE 222 // rose
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT_preonic_grid(
         QK_GESC,        KC_1,            KC_2,    KC_3,    KC_4,       KC_5,     KC_6,     KC_7,       KC_8,     KC_9,       KC_0,       KC_DEL,
         KC_TAB,         KC_Q,            KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,     KC_O,       KC_P,       KC_BSPC,
-        LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,     KC_L,       KC_SCLN,  KC_ENTER,
-        KC_LSFT,      KC_Z,            KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   KC_QUOTE,
-        MO(_NUMERIC),   LCTL(KC_LSFT), KC_LALT, KC_LGUI, MO(_LOWER), KC_SPACE, KC_SPACE, MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
+        LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,     KC_L,       KC_SCLN,    KC_ENTER,
+        KC_LSFT,        KC_Z,            KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   RSFT_T(KC_QUOTE),
+        MO(_NUMERIC),   LCTL(KC_LSFT),   KC_LALT, KC_LGUI, MO(_LOWER), KC_SPACE, KC_SPACE, MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
     ),
 	[_LOWER] = LAYOUT_preonic_grid(
-        KC_GRV,         KC_EXLM,         KC_AT,   KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,  KC_LPRN,    KC_RPRN,    KC_INS,
-        KC_TILD,        KC_EXLM,         KC_AT,   KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,  RALT(KC_P), RALT(KC_Q), _______,
+         KC_GRV,        KC_EXLM,         KC_AT,   KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,  KC_LPRN,    KC_RPRN,    KC_INS,
+         KC_GRV,        KC_EXLM,         KC_AT,   KC_HASH, KC_DLR,     KC_PERC,  KC_CIRC,  KC_AMPR,    KC_ASTR,  RALT(KC_P), RALT(KC_Q), _______,
         _______,        _______,         _______, KC_LPRN, KC_RPRN,    KC_PIPE,  KC_PLUS,  KC_MINS,    KC_UNDS,  KC_EQL,     RALT(KC_W), _______,
-        _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    KC_BSLS,    KC_GRV,
+        _______,        _______,         _______, _______, _______,    _______,  _______,  DBLCOLN,    _______,  _______,    KC_BSLS,    KC_GRV,
         _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    KC_HOME,  KC_PGDN,    KC_PGUP,    KC_END
     ),
 	[_RAISE] = LAYOUT_preonic_grid(
-        _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    _______,    _______,
-        _______,        KC_1,            KC_2,    KC_3,    KC_4,       KC_5,     KC_6,     KC_7,       KC_8,     KC_9,       KC_0,       _______,
+        KC_TILD,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    _______,    _______,
+        KC_TILD,        KC_1,            KC_2,    KC_3,    KC_4,       KC_5,     KC_6,     KC_7,       KC_8,     KC_9,       KC_0,       _______,
         _______,        _______,         _______, KC_LCBR, KC_RCBR,    KC_LABK,  KC_RABK,  KC_LBRC,    KC_RBRC,  KC_QUOT,    KC_DQUO,    _______,
         _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    _______,    _______,
         _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    KC_HOME,  KC_PGDN,    KC_PGUP,    KC_END
@@ -107,6 +111,17 @@ const rgblight_segment_t* const PROGMEM krig_rgb_layers[] = RGBLIGHT_LAYERS_LIST
 
 void keyboard_post_init_user(void) {
     rgblight_layers = krig_rgb_layers;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case DBLCOLN:
+            if (record->event.pressed) {
+                SEND_STRING("::");
+            }
+            return false;
+    }
+    return true;
 }
 
 bool led_update_user(led_t led_state) {
