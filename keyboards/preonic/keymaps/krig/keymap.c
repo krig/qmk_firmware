b0c09aadeab4ba7f8028b507701815645e6a9fe4
Kristoffer Grönlund <krig@koru.se>
2023-03-20 11:05:52 +0100

    align preonic keymap with moonlander evolution

--- keyboards/preonic/keymaps/krig/keymap.c
+++ keyboards/preonic/keymaps/krig/keymap.c
@@ -5,8 +5,11 @@
 
 
 enum custom_keycodes {
-    DBLCOLN = SAFE_RANGE, // ::
-    HEXPFIX // 0x
+    SQ_COLN = SAFE_RANGE, // ::
+    SQ_AMPR,
+    SQ_PIPE,
+    SQ_PATH,
+    SQ_PHEX // 0x
 };
 
 
@@ -22,25 +25,25 @@ enum preonic_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_preonic_grid(
-      QK_GESC,        KC_1,            KC_2,    KC_3,    KC_4,       KC_5,     KC_6,     KC_7,       KC_8,     KC_9,       KC_0,       KC_DEL,
+      QK_GESC,        KC_1,            KC_2,    KC_3,    KC_4,       KC_5,     KC_6,     KC_7,       KC_8,     KC_9,       KC_0,       KC_BSLS,
       KC_TAB,         KC_Q,            KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,     KC_O,       KC_P,       KC_BSPC,
       LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,     KC_L,       KC_SCLN,    KC_ENTER,
       KC_LSFT,        KC_Z,            KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMMA, KC_DOT,     KC_SLASH,   KC_QUOTE,
       MO(_NUMERIC),   LCTL(KC_LSFT),   KC_LALT, KC_LGUI, MO(_LOWER), KC_SPACE, KC_SPACE, MO(_RAISE), KC_LEFT,  KC_DOWN,    KC_UP,      KC_RIGHT
     ),
     [_LOWER] = LAYOUT_preonic_grid(
-       KC_GRV,        KC_EXLM,           KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,  KC_CIRC, KC_AMPR,    KC_ASTR,     KC_LPRN,    KC_RPRN,  KC_INS,
-      _______,           KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,        KC_9,       KC_0, RALT(KC_W),
-      _______,        _______,         KC_UNDS,    _______, _______,    _______,  _______, KC_UNDS, _______,  _______, RALT(KC_P), RALT(KC_Q),
-      _______,        _______,         _______,    _______, _______,    _______,  _______, _______,LGUI(KC_MINS),LGUI(KC_PLUS),KC_BSLS,  KC_GRV,
+      _______,        _______,         _______,    _______, _______,    _______,  _______, _______, _______,  _______, _______, KC_INS,
+      _______,        _______,           KC_P7,      KC_P8,   KC_P9,    _______,  _______, SQ_AMPR, _______,  _______, RALT(KC_W), KC_DEL,
+      _______,        SQ_PHEX,           KC_P4,      KC_P5,   KC_P6,    _______,  SQ_PIPE, SQ_COLN, _______,  RALT(KC_P), RALT(KC_Q), _______,
+      _______,          KC_P0,           KC_P1,      KC_P2,   KC_P3,    _______,  _______, SQ_PATH,LGUI(KC_MINS),LGUI(KC_PLUS),KC_BSLS,  KC_GRV,
       _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    KC_HOME,     KC_PGDN,    KC_PGUP,  KC_END
     ),
     [_RAISE] = LAYOUT_preonic_grid(
-      KC_TILD,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    _______,    _______,
-      _______,        KC_QUOT,           KC_LT,      KC_GT, KC_DQUO,      KC_AT,  KC_AMPR, DBLCOLN,    KC_LBRC,  KC_RBRC,    KC_PERC,    KC_DEL,
-      _______,        KC_EXLM,         KC_MINS,    KC_PLUS,  KC_EQL,    KC_HASH,  KC_PIPE, KC_COLN,    KC_LPRN,  KC_RPRN,    KC_QUES,    KC_INS,
-      _______,        KC_CIRC,         KC_SLSH,    KC_ASTR, KC_BSLS,    KC_UNDS,  KC_TILD,  KC_DLR,    KC_LCBR,  KC_RCBR,    _______,   _______,
-      _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    KC_HOME,  KC_PGDN,    KC_PGUP,    KC_END
+      _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  _______,    _______,    _______,
+      _______,        KC_TILD,           KC_LT,      KC_GT, KC_DQUO,    KC_QUOT,   KC_GRV, KC_AMPR,    KC_LBRC,  KC_RBRC,    KC_PERC,  _______,
+      _______,        KC_EXLM,         KC_MINS,    KC_PLUS,  KC_EQL,    KC_UNDS,  KC_PIPE, KC_COLN,    KC_LPRN,  KC_RPRN,    KC_QUES,  _______,
+      _______,        KC_CIRC,         KC_SLSH,    KC_ASTR, KC_BSLS,    KC_HASH,  KC_TILD,  KC_DLR,    KC_LCBR,  KC_RCBR,      KC_AT,   KC_GRV,
+      _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    _______,  _______,    _______,  _______
     ),
     [_ADJUST] = LAYOUT_preonic_grid(
       KC_SCRL,        KC_F1,           KC_F2,   KC_F3,   KC_F4,      KC_F5,    KC_F6,    KC_F7,      KC_F8,    KC_F9,      KC_F10,     KC_F11,
@@ -61,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  KC_1,       KC_2,       KC_3,
       _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  KC_4,       KC_5,       KC_6,
       _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    _______,  KC_7,       KC_8,       KC_9,
-      _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    HEXPFIX,  KC_DOT,     KC_0,       KC_COMMA
+      _______,        _______,         _______, _______, _______,    _______,  _______,  _______,    SQ_PHEX,  KC_DOT,     KC_0,       KC_COMMA
     )
 };
 
@@ -116,12 +119,27 @@ void keyboard_post_init_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
-        case DBLCOLN:
+        case SQ_COLN:
             if (record->event.pressed) {
                 SEND_STRING("::");
             }
             return false;
-        case HEXPFIX:
+        case SQ_PIPE:
+            if (record->event.pressed) {
+                SEND_STRING("||");
+            }
+            return false;
+        case SQ_AMPR:
+            if (record->event.pressed) {
+                SEND_STRING("&&");
+            }
+            return false;
+        case SQ_PATH:
+            if (record->event.pressed) {
+                SEND_STRING("../");
+            }
+            return false;
+        case SQ_PHEX:
             if (record->event.pressed) {
                 SEND_STRING("0x");
             }
