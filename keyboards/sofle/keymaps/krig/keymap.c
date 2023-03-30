428cb76217f5282cc8be2ccbd57926f72865ccb2
Kristoffer Grönlund <k@ziran.se>
2023-03-30 22:24:46 +0200

    sofle bugfixes

--- keyboards/sofle/keymaps/krig/keymap.c
+++ keyboards/sofle/keymaps/krig/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,  _______, _______,  KC_INS,
   _______, _______,   KC_P7,   KC_P8,   KC_P9, _______,                   _______, SQ_AMPR, KC_UNDS,  KC_HASH, _______,  KC_DEL,
   _______, SQ_PHEX,   KC_P4,   KC_P5,   KC_P6, _______,                   SQ_PIPE, SQ_COLN,   SQ_AA,    SQ_OE,   SQ_AE,  KC_GRV,
-  _______,   KC_P0,   KC_P1,   KC_P2,   KC_P3, _______, _______, _______, _______, SQ_PATH, ZOOM_UT,  ZOOM_IN, KC_BSLS, _______,
+  _______,   KC_P0,   KC_P1,   KC_P2,   KC_P3, _______, _______, _______, _______, SQ_PATH, ZOOM_UT,  ZOOM_IN, KC_BSLS, KC_RGHT,
                     _______, _______, _______, _______, _______,  KC_TAB, _______, _______, KC_PGDN,  KC_PGUP
 ),
 [_SYMBOLS] = LAYOUT(
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_CIRC,   KC_LT,   KC_GT, KC_DQUO, KC_QUOT,                    KC_GRV, KC_AMPR, KC_LBRC, KC_RBRC, KC_PERC, _______,
   _______, KC_EXLM, KC_MINS, KC_PLUS,  KC_EQL, KC_UNDS,                   KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_QUES,  KC_GRV,
   _______, KC_TILD, KC_SLSH, KC_ASTR, KC_BSLS, KC_HASH, _______, _______, KC_TILD,  KC_DLR, KC_LBRC, KC_RBRC,   KC_AT, _______,
-           _______, _______, _______, _______, _______, KC_BSPC, _______, _______, KC_LEFT, KC_RGHT
+                    _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______, _______
 ),
 [_GAME] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
