f99f7d0acd771e3ac9ba76b5e5926de6e271c6a7
Kristoffer Grönlund <k@ziran.se>
2023-05-23 23:18:54 +0200

    screen tweaks

--- keyboards/sofle/keymaps/krig/keymap.c
+++ keyboards/sofle/keymaps/krig/keymap.c
@@ -141,7 +141,7 @@ static void print_status_narrow(void) {
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
-        return OLED_ROTATION_180;
+        return OLED_ROTATION_270;
     }
     return rotation;
 }
