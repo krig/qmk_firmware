ab62d89e92f1165ce2007c760306d9583ef3051f
Kristoffer Grönlund <k@ziran.se>
2023-08-04 21:44:50 +0200

    iterating on middlemajk / my middlemak-nh mod

--- users/krig/layout/middlemakmodkg.h
+++ users/krig/layout/middlemakmodkg.h
@@ -1,15 +1,15 @@
 #pragma once
 // Middlemak-ModKG
 // https://github.com/krig
-// q w l d g  / f u o y
-// r s n t p  m h e a i
-// z x c v b  j k , . '
+// q w l d b  y f o u '
+// r s n t g  m h e i a
+// z x c p v  j k , . /
 
-#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_D, KC_G
-#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_C, KC_V, KC_B
+#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_D, KC_B
+#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
+#define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_C, KC_P, KC_V
 
-#define ALTERN_R1 KC_SLSH, KC_F, KC_U, KC_O, KC_Y
+#define ALTERN_R1 KC_Y, KC_F, KC_O, KC_U, KC_QUOT
 #define ALTERN_R2 KC_M, KC_H, KC_E, KC_A, KC_I
-#define ALTERN_R3 KC_J, KC_K, KC_COMM, KC_DOT, KC_QUOT
+#define ALTERN_R3 KC_J, KC_K, KC_COMM, KC_DOT, KC_SLSH
 
