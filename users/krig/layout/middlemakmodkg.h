9f3b43d9420099f39703f71b9f5eee929e2c4811
Kristoffer Grönlund <k@ziran.se>
2023-08-04 22:02:18 +0200

    bugfixing

--- users/krig/layout/middlemakmodkg.h
+++ users/krig/layout/middlemakmodkg.h
@@ -5,11 +5,11 @@
 // r s n t g  m h e i a
 // z x c p v  j k , . /
 
-#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_D, KC_B
+#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_D, KC_V
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
-#define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_C, KC_P, KC_V
+#define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_C, KC_P, KC_B
 
 #define ALTERN_R1 KC_Y, KC_F, KC_O, KC_U, KC_QUOT
-#define ALTERN_R2 KC_M, KC_H, KC_E, KC_A, KC_I
+#define ALTERN_R2 KC_M, KC_H, KC_E, KC_I, KC_A
 #define ALTERN_R3 KC_J, KC_K, KC_COMM, KC_DOT, KC_SLSH
 
