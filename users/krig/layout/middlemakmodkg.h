381229aaf012073ef37c1839029601461d01c6d3
Kristoffer Grönlund <k@ziran.se>
2023-08-05 21:24:38 +0200

    not even sure what I changed now

--- users/krig/layout/middlemakmodkg.h
+++ users/krig/layout/middlemakmodkg.h
@@ -9,7 +9,7 @@
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
 #define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_L, KC_D, KC_B
 
-#define ALTERN_R1 KC_Y, KC_F, KC_O, KC_U, KC_QUOT
-#define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_J, KC_M, KC_SLSH, KC_DOT, KC_COMM
+#define ALTERN_R1 KC_K, KC_F, KC_O, KC_U, KC_QUOT
+#define ALTERN_R2 KC_Y, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_J, KC_M, KC_COMM, KC_DOT, KC_SLSH
 
