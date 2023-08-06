881db4c4523a5f65544c6452cf957ec4c52f6080
Kristoffer Grönlund <k@ziran.se>
2023-08-06 20:13:21 +0200

    improvements

--- users/krig/layout/cmpmak.h
+++ users/krig/layout/cmpmak.h
@@ -3,14 +3,14 @@
 //
 // experimental layout
 //
-// q c m p b z k o y '
-// r s n t g u h e i a
-// w f l d v x j , . ;
+// q c m p b z u o y '
+// r s n t g j h e i a
+// w f l d v x k , . ;
 
 #define ALTERN_L1 KC_Q, KC_C, KC_M, KC_P, KC_B
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
 #define ALTERN_L3 LT(_LANG, KC_W), KC_F, KC_L, KC_D, KC_V
-#define ALTERN_R1 KC_Z, KC_K, KC_O, KC_Y, KC_QUOT
-#define ALTERN_R2 KC_U, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_X, KC_J, KC_COMM, KC_DOT, KC_SLSH
+#define ALTERN_R1 KC_Z, KC_U, KC_O, KC_Y, KC_QUOT
+#define ALTERN_R2 KC_J, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_X, KC_K, KC_COMM, KC_DOT, KC_SLSH
 
