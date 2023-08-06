26f31fa37d0c5493ae9b7fd9a8c13cb1a10e3761
Kristoffer Grönlund <k@ziran.se>
2023-08-06 23:47:33 +0200

    figured out that I don't want layer shifts to cancel oneshot mods

--- users/krig/features/oneshot.c
+++ users/krig/features/oneshot.c
@@ -1,5 +1,12 @@
 #include "krig.h"
 #include "oneshot.h"
+#include "print.h"
+
+static void print_info(const char *msg, oneshot_state *state, uint16_t mod, uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
+    uprintf("%s OS: 0x%x, mod: 0x%04x, kc: 0x%04x, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", msg, *state, mod, keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+}
 
 void update_oneshot(
     oneshot_state *state,
@@ -13,6 +20,9 @@ void update_oneshot(
             // Trigger keydown
             if (*state == os_up_unqueued) {
                 register_code(mod);
+                print_info("r->os_down_unused", state, mod, keycode, record);
+            } else {
+                print_info("->os_down_unused", state, mod, keycode, record);
             }
             *state = os_down_unused;
         } else {
@@ -21,10 +31,12 @@ void update_oneshot(
             case os_down_unused:
                 // If we didn't use the mod while trigger was held, queue it.
                 *state = os_up_queued;
+                print_info("->os_up_queued", state, mod, keycode, record);
                 break;
             case os_down_used:
                 // If we did use the mod while trigger was held, unregister it.
                 *state = os_up_unqueued;
+                print_info("u->os_up_unqueued", state, mod, keycode, record);
                 unregister_code(mod);
                 break;
             default:
@@ -37,15 +49,18 @@ void update_oneshot(
                 // Cancel oneshot on designated cancel keydown.
                 *state = os_up_unqueued;
                 unregister_code(mod);
+                print_info("u->os_up_unqueued (cancel)", state, mod, keycode, record);
             }
             if (!is_oneshot_ignored_key(keycode)) {
                 switch (*state) {
                     case os_down_unused:
                         *state = os_up_queued_used;
+                        print_info("->os_up_queued_used", state, mod, keycode, record);
                         break;
                     case os_up_queued_used:
                         *state = os_up_unqueued;
                         unregister_code(mod);
+                        print_info("u->os_up_unqueued", state, mod, keycode, record);
                         break;
                     default:
                         break;
@@ -57,14 +72,17 @@ void update_oneshot(
                 switch (*state) {
                     case os_down_unused:
                         *state = os_down_used;
+                        print_info("->os_down_used", state, mod, keycode, record);
                         break;
                     case os_up_queued:
                         *state = os_up_unqueued;
                         unregister_code(mod);
+                        print_info("u queued->os_up_unqueued", state, mod, keycode, record);
                         break;
                     case os_up_queued_used:
                         *state = os_up_unqueued;
                         unregister_code(mod);
+                        print_info("u queued_qued->os_up_unqueued", state, mod, keycode, record);
                         break;
                     default:
                         break;
@@ -171,11 +189,11 @@ void process_record_oneshot(oneshot_context* ctx, uint16_t keycode, keyrecord_t*
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
-    case THUMB_L1:
-    case THUMB_L2:
-    case THUMB_R1:
-    case THUMB_R2:
-        return true;
+    //case THUMB_L1:
+    //case THUMB_L2:
+    //case THUMB_R1:
+    //case THUMB_R2:
+    //return true;
     default:
         return false;
     }
