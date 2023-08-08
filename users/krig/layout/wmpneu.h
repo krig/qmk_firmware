36a5acceab823775c8593dab578c9f99f133b754
Kristoffer Grönlund <k@ziran.se>
2023-08-08 23:47:18 +0200

    swap w and c

--- users/krig/layout/wmpneu.h
+++ users/krig/layout/wmpneu.h
@@ -3,13 +3,13 @@
 //
 // experimental layout
 //
-// q w m p v x y o u /
+// q c m p v x y o u /
 // r s n t g f h e i a
-// z c l d b j k ' . ,
+// z w l d b j k ' . ,
 
-#define ALTERN_L1 KC_Q, KC_W, KC_M, KC_P, KC_V
+#define ALTERN_L1 KC_Q, KC_C, KC_M, KC_P, KC_V
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
-#define ALTERN_L3 LT(_LANG, KC_Z), KC_C, KC_L, KC_D, KC_B
+#define ALTERN_L3 LT(_LANG, KC_Z), KC_W, KC_L, KC_D, KC_B
 #define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_SLSH
 #define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
 #define ALTERN_R3 KC_J, KC_F, KC_QUOT, KC_DOT, KC_COMM
