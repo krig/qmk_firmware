f9581d72a525722835023a5c62d42b420fb3c2f4
Kristoffer Grönlund <k@ziran.se>
2023-08-22 06:03:30 +0200

    a new version of wmpneu

--- users/krig/layout/kanary.h
+++ users/krig/layout/kanary.h
@@ -10,7 +10,7 @@
 #define ALTERN_L1 KC_W, KC_L, KC_Y, KC_P, KC_B
 #define ALTERN_L2 KC_C, KC_R, KC_S, KC_T, KC_K
 #define ALTERN_L3 LT(_LANG, KC_V), KC_X, KC_G, KC_D, KC_J
-#define ALTERN_R1 KC_Z, KC_F, KC_O, KC_U, KC_QOUT
+#define ALTERN_R1 KC_Z, KC_F, KC_O, KC_U, KC_QUOT
 #define ALTERN_R2 KC_M, KC_N, KC_E, KC_I, KC_A
 #define ALTERN_R3 KC_Q, KC_H, KC_SLSH, KC_DOT, KC_COMM
 
--- users/krig/layout/wmpneu.h
+++ users/krig/layout/wmpneu.h
@@ -3,13 +3,13 @@
 //
 // experimental layout
 //
-// q c m p b y f o u /
-// r s n t g j h e i a
-// z w l d v x k ' . ,
+// w c m g p q y o u '
+// r s n t v k h e i a
+// z x l d b j f / . ,
 
-#define ALTERN_L1 KC_Q, KC_C, KC_M, KC_P, KC_V
-#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
-#define ALTERN_L3 LT(_LANG, KC_Z), KC_W, KC_L, KC_D, KC_B
-#define ALTERN_R1 KC_Z, KC_Y, KC_O, KC_U, KC_SLSH
-#define ALTERN_R2 KC_J, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_X, KC_K, KC_QUOT, KC_DOT, KC_COMM
+#define ALTERN_L1 KC_W, KC_C, KC_M, KC_G, KC_P
+#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_V
+#define ALTERN_L3 LT(_LANG, KC_Z), KC_X, KC_L, KC_D, KC_B
+#define ALTERN_R1 KC_Q, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_J, KC_F, KC_SLSH, KC_DOT, KC_COMM
