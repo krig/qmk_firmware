acc9b383e51189ec938330f1bf338f21c01e6d2c
Kristoffer Grönlund <k@ziran.se>
2023-08-25 18:18:24 +0200

    reversing some experimental changes

--- users/krig/layout/wmpneu.h
+++ users/krig/layout/wmpneu.h
@@ -3,13 +3,13 @@
 //
 // experimental layout
 //
-// x c m g v q y o u '
-// r s n t p k h e i a
-// z w l d b j f / . ,
+// x c m p v y f o u '
+// r s n t g k h e i a
+// q w l d b z j / . ,
 
-#define ALTERN_L1 KC_X, KC_C, KC_M, KC_G, KC_V
-#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define ALTERN_L3 LT(_LANG, KC_Z), KC_W, KC_L, KC_D, KC_B
-#define ALTERN_R1 KC_Q, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_L1 KC_X, KC_C, KC_M, KC_P, KC_V
+#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
+#define ALTERN_L3 LT(_LANG, KC_Q), KC_W, KC_L, KC_D, KC_B
+#define ALTERN_R1 KC_Y, KC_F, KC_O, KC_U, KC_QUOT
 #define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_J, KC_F, KC_SLSH, KC_DOT, KC_COMM
+#define ALTERN_R3 KC_Z, KC_J, KC_SLSH, KC_DOT, KC_COMM
