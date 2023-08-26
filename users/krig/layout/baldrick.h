e1c518cd03ceee7ec1e199bcaa1851f4431b9404
Kristoffer Grönlund <k@ziran.se>
2023-08-26 21:17:37 +0200

    add baldrick

---
+++ users/krig/layout/baldrick.h
@@ -0,0 +1,17 @@
+#pragma once
+// baldrick
+//
+// experimental layout
+//
+// b l d c q z f o u ,
+// n r t s g y h a e i
+// p j m w v x k ' - .
+
+#define ALTERN_L1 KC_B, KC_L, KC_D, KC_C, KC_Q
+#define ALTERN_L2 KC_N, KC_R, KC_T, KC_S, KC_G
+#define ALTERN_L3 KC_P, KC_J, KC_M, KC_W, LT(_LANG, KC_V)
+
+#define ALTERN_R1 KC_Z, KC_F, KC_O, KC_U, KC_COMM
+#define ALTERN_R2 KC_Y, KC_H, KC_A, KC_E, KC_I
+#define ALTERN_R3 KC_X, KC_K, KC_QUOT, KC_MINS, KC_DOT
+
