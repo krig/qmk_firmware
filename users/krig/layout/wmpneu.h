8fa1b8fbec3c1fa5b323002a36d1d98731813f89
Kristoffer Grönlund <k@ziran.se>
2023-09-13 21:32:56 +0200

    move x to top left

--- users/krig/layout/wmpneu.h
+++ users/krig/layout/wmpneu.h
@@ -3,16 +3,16 @@
 //
 // experimental layout
 //
-// q c m g z  x y o u '
+// x c m g q  z y o u '
 // r s n t p  k h e i a
 // b w l d v  / f j . ,
 // *
 //
 // *: swedish layer on hold
 
-#define ALTERN_L1 KC_Q, KC_C, KC_M, KC_G, KC_Z
+#define ALTERN_L1 KC_X, KC_C, KC_M, KC_G, KC_Q
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
 #define ALTERN_L3 K_LANG(KC_B), KC_W, KC_L, KC_D, KC_V
-#define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_R1 KC_Z, KC_Y, KC_O, KC_U, KC_QUOT
 #define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
 #define ALTERN_R3 KC_SLSH, KC_F, KC_J, KC_DOT, KC_COMM
