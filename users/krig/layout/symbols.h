b8893cace54340554f7893cc505c573412105817
Kristoffer Grönlund <k@ziran.se>
2023-09-15 21:59:43 +0200

    spacing

--- users/krig/layout/symbols.h
+++ users/krig/layout/symbols.h
@@ -3,11 +3,15 @@
 // inspired by https://getreuer.info/posts/keyboards/symbol-layer/index.html
 
 #define SYMBOLS_L0 _______, SQ_LTLT, SQ_GTGT, SQ_PATH, _______
+
 #define SYMBOLS_L1  KC_GRV,   KC_LT,   KC_GT, KC_SLSH, KC_PERC
 #define SYMBOLS_L2 KC_EXLM, KC_MINS, KC_UNDS,  KC_EQL, KC_TILD
 #define SYMBOLS_L3 KC_QUES, KC_PLUS, KC_ASTR, KC_HASH,   KC_AT
 
+
+
 #define SYMBOLS_R0 SQ_PIPE, SQ_AMPR, SQ_LTAR, SQ_RTAR, _______
+
 #define SYMBOLS_R1 SQ_COLN, KC_AMPR, KC_LBRC, KC_RBRC, SQ_EXPI
 #define SYMBOLS_R2 KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_SCLN
 #define SYMBOLS_R3 KC_CIRC,  KC_DLR, KC_LCBR, KC_RCBR, KC_BSLS
