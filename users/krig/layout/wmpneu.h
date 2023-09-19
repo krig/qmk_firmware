b415b98a161761d0f42f4e7ec5e8e1414434023f
Kristoffer Grönlund <k@ziran.se>
2023-09-19 20:12:18 +0200

    revision yet again

--- users/krig/layout/wmpneu.h
+++ users/krig/layout/wmpneu.h
@@ -3,16 +3,17 @@
 //
 // experimental layout
 //
-// x c m p q* z f o u '
-// r s n t g  y h e i a
-// b w l d v  " k j . ,
+// x c m g z  q y o u '
+// r s n t p  k h e i a
+// b w l d v  j f " , .
+// *
 //
 // *: swedish layer on hold
 
-#define ALTERN_L1 KC_X, KC_C, KC_M, KC_P, KC_Q
-#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_G
+#define ALTERN_L1 KC_X, KC_C, KC_M, KC_G, KC_Z
+#define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
 #define ALTERN_L3 K_LANG(KC_B), KC_W, KC_L, KC_D, KC_V
 
-#define ALTERN_R1 KC_Z, KC_F, KC_O, KC_U, KC_QUOT
-#define ALTERN_R2 KC_Y, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_DQUO, KC_K, KC_J, KC_DOT, KC_COMM
+#define ALTERN_R1 KC_Q, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_J, KC_F, KC_DQUO, KC_COMM, KC_DOT
