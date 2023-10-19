2e64e0b812005e9a9f5ec2c9f1ab9ca382eb63f1
Kristoffer Grönlund <k@ziran.se>
2023-10-19 08:10:58 +0200

    move shift to offload ring finger

--- users/krig/layout/waffle.h
+++ users/krig/layout/waffle.h
@@ -3,8 +3,8 @@
 //
 // experimental layout
 /*   q c m g z  x y o u , */
-/*   r s n t p  j h a e i */
-/*   w f l d b  v k ' . / */
+/*   r s n t p  / h a e i */
+/*   w f l d b  v k ' j . */
 #ifndef ALTERN_L1
 
 #define ALTERN_L1 KC_Q, KC_C, KC_M, KC_G, KC_Z
@@ -12,8 +12,8 @@
 #define ALTERN_L3 KC_W, KC_F, KC_L, KC_D, KC_B
 
 #define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_COMM
-#define ALTERN_R2 KC_J, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_V, KC_K, KC_QUOT, KC_DOT, OSM_SFT
+#define ALTERN_R2 OSM_SFT, KC_H, KC_A, KC_E, KC_I
+#define ALTERN_R3 KC_V, KC_K, KC_QUOT, KC_J, KC_DOT
 
 #endif
 
