1435adf49470099e05881f2970f8d447df4badb9
Kristoffer Grönlund <k@ziran.se>
2023-10-19 08:50:38 +0200

    various bigram optimizations and tweaks

--- users/krig/layout/waffle.h
+++ users/krig/layout/waffle.h
@@ -2,18 +2,18 @@
 // waffle
 //
 // experimental layout
-/*   q c m g v  z y o u , */
-/*   r s n t p  / h a e i */
-/*   w f l d b  x k ' j . */
+/*   q c l g z  x f o u , */
+/*   r s n t p  k h a e i */
+/*   w v m d b  / y ' j . */
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_Q, KC_C, KC_M, KC_G, KC_V
+#define ALTERN_L1 KC_Q, KC_C, KC_L, KC_G, KC_Z
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define ALTERN_L3 KC_W, KC_F, KC_L, KC_D, KC_B
+#define ALTERN_L3 KC_W, KC_V, KC_M, KC_D, KC_B
 
-#define ALTERN_R1 KC_Z, KC_Y, KC_O, KC_U, KC_COMM
-#define ALTERN_R2 OSM_SFT, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_X, KC_K, KC_QUOT, KC_J, KC_DOT
+#define ALTERN_R1 KC_X, KC_F, KC_O, KC_U, KC_COMM
+#define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
+#define ALTERN_R3 OSM_SFT, KC_Y, KC_QUOT, KC_J, KC_DOT
 
 #endif
 
