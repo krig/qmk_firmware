7d422f3bbb2f91ee4a19c4d8194968904d903fc3
Kristoffer Grönlund <k@ziran.se>
2023-10-19 09:27:07 +0200

    bl bigram screwed this variant..

--- users/krig/layout/waffle.h
+++ users/krig/layout/waffle.h
@@ -2,14 +2,14 @@
 // waffle
 //
 // experimental layout
-/*   q c l g z  x f o u , */
+/*   q c m g z  x f o u , */
 /*   r s n t p  k h a e i */
-/*   w v m d b  / y ' j . */
+/*   w v l d b  / y ' j . */
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_Q, KC_C, KC_L, KC_G, KC_Z
+#define ALTERN_L1 KC_Q, KC_C, KC_M, KC_G, KC_Z
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define ALTERN_L3 KC_W, KC_V, KC_M, KC_D, KC_B
+#define ALTERN_L3 KC_W, KC_V, KC_L, KC_D, KC_B
 
 #define ALTERN_R1 KC_X, KC_F, KC_O, KC_U, KC_COMM
 #define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
