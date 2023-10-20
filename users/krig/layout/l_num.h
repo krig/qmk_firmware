82940282ad6610715904e1fc7732a6fa6ba73ec4
Kristoffer Grönlund <k@ziran.se>
2023-10-20 10:28:44 +0200

    Add layer lock to num layer

--- users/krig/layout/l_num.h
+++ users/krig/layout/l_num.h
@@ -4,7 +4,7 @@
 #define L_NUM_L2    KC_0, KC_4,    KC_5,    KC_6,       KC_J
 #define L_NUM_L3 SQ_PHEX, KC_7,    KC_8,    KC_9,    S(KC_G)
 
-#define L_NUM_R1 _______, _______, _______, _______, _______
+#define L_NUM_R1 _______, _______, _______, _______,   LLOCK
 #define L_NUM_R2 _______, KC_LSFT, KC_LGUI, KC_LCTL, KC_LALT
 #define L_NUM_R3 _______,  KC_DOT, KC_COMM, _______, _______
 
