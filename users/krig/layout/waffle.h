bdcd7e00471f22d82186972379b33bf46c710b4c
Kristoffer Grönlund <k@ziran.se>
2023-10-20 10:24:40 +0200

    rearrange punctuation a bit

--- users/krig/layout/waffle.h
+++ users/krig/layout/waffle.h
@@ -2,18 +2,18 @@
 // waffle
 //
 // experimental layout
-/*   q c m g z  x y o u , */
+/*   q c m g z  x y o u j */
 /*   r s n t p  k h a e i */
-/*   w v l d b  / f ' j . */
+/*   w v l d b  / f . ' , */
 #ifndef ALTERN_L1
 
 #define ALTERN_L1 KC_Q, KC_C, KC_M, KC_G, KC_Z
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
 #define ALTERN_L3 KC_W, KC_V, KC_L, KC_D, KC_B
 
-#define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_COMM
+#define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_J
 #define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_SLSH, KC_F, KC_QUOT, KC_J, KC_DOT
+#define ALTERN_R3 KC_SLSH, KC_F, KC_DOT, KC_QUOT, KC_COMM
 
 #endif
 
