5e398faf6306bd3f50bb5fd4346e1d3665d63124
Kristoffer Grönlund <k@ziran.se>
2023-10-20 10:38:19 +0200

    revisions

--- users/krig/layout/l_nav.h
+++ users/krig/layout/l_nav.h
@@ -1,10 +1,10 @@
 // nav layer
 
-#define L_NAV_L1   LLOCK, CW_TOGG, CMD_GRV, CMD_TAB,  LCTL_W
+#define L_NAV_L1   LLOCK, GUI_SPC, CMD_GRV, CMD_TAB,  LCTL_W
 #define L_NAV_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_RALT
-#define L_NAV_L3  GUI__Z,  GUI__X,  GUI__C,  GUI__V, GUI_SPC
+#define L_NAV_L3  GUI__Z,  GUI__X,  GUI__C,  GUI__V, _______
 
-#define L_NAV_R1  KC_INS, KC_HOME,   KC_UP,  KC_END, KC_WFWD
+#define L_NAV_R1  KC_INS, KC_HOME,   KC_UP,  KC_END, _______
 #define L_NAV_R2  KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT,  SWE_AA
 #define L_NAV_R3 MO(_NUM), KC_PGDN, KC_PGUP,  SWE_AE,  SWE_OE
 
