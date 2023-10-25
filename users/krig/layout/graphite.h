8fa9cf467509066d7c9ffeb6b1d31387c138c6d9
Kristoffer Grönlund <k@ziran.se>
2023-10-25 12:20:09 +0200

    fix graphite

--- users/krig/layout/graphite.h
+++ users/krig/layout/graphite.h
@@ -2,15 +2,15 @@
 // graphite
 //
 // by rdavison
-//~ ! @ # $ %  ^ & * ( ) { }
-//  B L D W Z  _ F O U J : + |
-//  N R T S G  Y H A E I ?
-//  Q X M C V  K P > " <
+// ~ ! @ # $ %  ^ & * ( ) { }
+//   B L D W Z  _ F O U J : + |
+//   N R T S G  Y H A E I ?
+//   Q X M C V  K P > " <
 //
-//` 1 2 3 4 5  6 7 8 9 0 [ ]
-//  b l d w z  ' f o u j ; = \
-//  n r t s g  y h a e i ,
-//  q x m c v  k p . - /
+// ` 1 2 3 4 5  6 7 8 9 0 [ ]
+//   b l d w z  ' f o u j ; =
+//   n r t s g  y h a e i ,
+//   q x m c v  k p . - /
 
 #ifndef ALTERN_L1
 
