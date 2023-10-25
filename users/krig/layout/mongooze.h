a7c44b365c93bc4fdff143934ac0e74c7662ca68
Kristoffer Grönlund <k@ziran.se>
2023-10-25 20:55:37 +0200

    updated mongooze

--- users/krig/layout/mongooze.h
+++ users/krig/layout/mongooze.h
@@ -1,18 +1,18 @@
 #pragma once
 // mongooze
 //
-// q w m g z x y o u ,
+// q w m g z ' y o u j
 // r s n t p k h a e i
-// j c l d b v f ' / .
+// x c l d b v f . / ,
 #ifndef ALTERN_L1
 
 #define ALTERN_L1 KC_Q, KC_W, KC_M, KC_G, KC_Z
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define ALTERN_L3 KC_J, KC_C, KC_L, KC_D, KC_B
+#define ALTERN_L3 KC_X, KC_C, KC_L, KC_D, KC_B
 
-#define ALTERN_R1 KC_X, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_J
 #define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_V, KC_F, KC_QUOT, KC_SLSH, KC_DOT
+#define ALTERN_R3 KC_V, KC_F, KC_DOT, KC_SLSH, KC_COMM
 
 #endif
 
