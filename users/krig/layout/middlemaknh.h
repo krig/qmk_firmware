28b3640e16933b0073d9f8d0ee528edce1256fa2
Kristoffer Grönlund <k@ziran.se>
2023-10-27 11:05:06 +0200

    modded middlemak-nh

--- users/krig/layout/middlemaknh.h
+++ users/krig/layout/middlemaknh.h
@@ -3,16 +3,16 @@
 // https://github.com/KeyboardLayout2/Middlemak-NH
 // q w l d g   j f o u '
 // n s r t p   y h e i a ä
-// z x c v b   k m , . å ö
+// z x c v b   k m å . , ö
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_D, KC_G
-#define ALTERN_L2 KC_N, KC_S, KC_R, KC_T, KC_P
-#define ALTERN_L3 KC_Z, KC_X, KC_C, KC_V, KC_B
+#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_P, KC_Z
+#define ALTERN_L2 KC_N, KC_S, KC_R, KC_T, KC_G
+#define ALTERN_L3 KC_X, KC_V, KC_C, KC_D, KC_B
 
 #define ALTERN_R1 KC_J, KC_F, KC_O, KC_U, KC_QUOT
 #define ALTERN_R2 KC_Y, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_K, KC_M, KC_COMM, KC_DOT, RALT(KC_W)
+#define ALTERN_R3 KC_K, KC_M, RALT(KC_W), KC_DOT, KC_COMM
 
 #endif
