12d62759d70dd1a5477501db84e5d34e46ffe0e1
Kristoffer Grönlund <k@ziran.se>
2023-10-31 15:43:50 +0100

    swap fm

--- users/krig/layout/middlemaknh.h
+++ users/krig/layout/middlemaknh.h
@@ -2,8 +2,8 @@
 // Middlemak NH (swedish mod)
 // https://github.com/KeyboardLayout2/Middlemak-NH
 // q w l p b   j y o u '
-// n s r t g   m h e i a ä
-// x v c d k   z f å . , ö
+// n s r t g   f h e i a ä
+// x v c d k   z m å . , ö
 
 #ifndef ALTERN_L1
 
@@ -12,7 +12,7 @@
 #define ALTERN_L3 KC_X, KC_V, KC_C, KC_D, KC_K
 
 #define ALTERN_R1 KC_J, KC_Y, KC_O, KC_U, KC_QUOT
-#define ALTERN_R2 KC_M, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_Z, KC_F, RALT(KC_W), KC_DOT, KC_COMM
+#define ALTERN_R2 KC_F, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_Z, KC_M, RALT(KC_W), KC_DOT, KC_COMM
 
 #endif
