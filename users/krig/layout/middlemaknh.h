13ebba4eaea3a3394df5023fa7e47b34403642b3
Kristoffer Grönlund <k@ziran.se>
2023-10-31 14:55:24 +0100

    updated middlemak variant

--- users/krig/layout/middlemaknh.h
+++ users/krig/layout/middlemaknh.h
@@ -1,18 +1,18 @@
 #pragma once
 // Middlemak NH (swedish mod)
 // https://github.com/KeyboardLayout2/Middlemak-NH
-// q w l d g   j f o u '
-// n s r t p   y h e i a ä
-// z x c v b   k m å . , ö
+// q w l p b   j y o u '
+// n s r t g   m h e i a ä
+// x v c d k   z f å . , ö
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_P, KC_Z
+#define ALTERN_L1 KC_Q, KC_W, KC_L, KC_P, KC_B
 #define ALTERN_L2 KC_N, KC_S, KC_R, KC_T, KC_G
-#define ALTERN_L3 KC_X, KC_V, KC_C, KC_D, KC_B
+#define ALTERN_L3 KC_X, KC_V, KC_C, KC_D, KC_K
 
-#define ALTERN_R1 KC_J, KC_F, KC_O, KC_U, KC_QUOT
-#define ALTERN_R2 KC_Y, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_K, KC_M, RALT(KC_W), KC_DOT, KC_COMM
+#define ALTERN_R1 KC_J, KC_Y, KC_O, KC_U, KC_QUOT
+#define ALTERN_R2 KC_M, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_Z, KC_F, RALT(KC_W), KC_DOT, KC_COMM
 
 #endif
