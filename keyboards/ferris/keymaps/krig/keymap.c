7f21f9aa75a4a5ccb9e359e4984affa4566cb21d
Kristoffer Grönlund <k@ziran.se>
2023-11-26 22:08:56 +0100

    number layer changes

--- keyboards/ferris/keymaps/krig/keymap.c
+++ keyboards/ferris/keymaps/krig/keymap.c
@@ -7,6 +7,7 @@
 #include "features/swapper.h"
 #include "features/layer_lock.h"
 
+#define TG_MOUS TG(_MOUSE)
 #define LAYOUT_wrapper(...)             LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_ADJUST] = LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
-    DF_QWER, TG(_MOUSE), _______, _______, _______, _______, _______, _______, _______, _______,
-    DF_GAME, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
+       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_10,
+    DF_QWER, DF_GAME, TG_MOUS, _______, _______, _______, _______, _______, _______, QK_BOOT,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
