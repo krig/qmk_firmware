0f7d365b3be2777ee29ebdec1b66014a45fe4b2d
Kristoffer Grönlund <k@ziran.se>
2023-11-29 09:25:51 +0100

    Add numpad layer for lily58

--- keyboards/lily58/keymaps/krig/keymap.c
+++ keyboards/lily58/keymaps/krig/keymap.c
@@ -23,21 +23,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,         NUMROW_L,                    NUMROW_R, KC_RALT,
    KC_TAB,        QWERTY_L1,                   QWERTY_R1, KC_BSPC,
   CTL_ESC,        QWERTY_L2,                   QWERTY_R2, KC_RCTL,
-  OSM_SFT,        QWERTY_L3, QK_REP,  QK_AREP, QWERTY_R3, KC_RSFT,
+  OSM_SFT,        QWERTY_L3, MO(_NUM),  QK_REP, QWERTY_R3, KC_RSFT,
   KC_LALT, KC_LGUI, THUMB_L2,  THUMB_L1, THUMB_R1, THUMB_R2, RGUI_T(KC_PGDN), RALT_T(KC_PGUP)
  ),
  [_ALTERN] = LAYOUT_wrapper(
    KC_GRV,         NUMROW_L,                    NUMROW_R, KC_RALT,
    KC_TAB,        ALTERN_L1,                   ALTERN_R1, KC_BSPC,
   CTL_ESC,        ALTERN_L2,                   ALTERN_R2, KC_RCTL,
-  OSM_SFT,        ALTERN_L3, QK_REP,  QK_AREP, ALTERN_R3, KC_RSFT,
+  OSM_SFT,        ALTERN_L3, MO(_NUM),  QK_REP, ALTERN_R3, KC_RSFT,
   KC_LALT, KC_LGUI, THUMB_L2,  THUMB_L1, THUMB_R1, THUMB_R2, RGUI_T(KC_PGDN), RALT_T(KC_PGUP)
  ),
  [_GAME] = LAYOUT_wrapper(
    KC_ESC,  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RALT,
    KC_TAB,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   KC_LCTL, KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,                      KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_RCTL,
-  KC_LSFT, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V, QK_REP,  QK_AREP,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+  KC_LSFT, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V, MO(_NUM),  QK_REP,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
                              KC_LALT, KC_LGUI, THUMB_L2,  THUMB_L1, THUMB_R1, THUMB_R2, RGUI_T(KC_PGDN), RALT_T(KC_PGUP)
  ),
  [_NAV] = LAYOUT_wrapper(
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, L_NUM_L1,                   L_NUM_R1, _______,
   _______, L_NUM_L2,                   L_NUM_R2, _______,
   _______, L_NUM_L3, _______, _______, L_NUM_R3, _______,
-                             _______, _______, _______, _______, KC_TAB, _______, _______, _______
+                             _______, _______, _______, _______, _______, _______, _______, _______
  ),
  [_SYM] = LAYOUT_wrapper(
   _______, L_SYM_L0,                   L_SYM_R0, _______,
