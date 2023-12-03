705fbf2a2d5c734a0aa8b4dd7aa5f963900031f4
Kristoffer Grönlund <k@ziran.se>
2023-12-03 11:01:02 +0100

    I

--- users/krig/layout/krigite.h
+++ users/krig/layout/krigite.h
@@ -3,9 +3,9 @@
 //
 // slight mod of graphite
 // by rdavison
-//   b l d w z  j k o u ,
-//   n r t s g  y h a e i
-//   q x m c v  p f ' - .
+//   b l d w z  ' y o u j
+//   n r t s g  k h a e i
+//   q x m c v  p f . - ,
 
 #ifndef ALTERN_L1
 
@@ -13,9 +13,9 @@
 #define ALTERN_L2 KC_N, KC_R, KC_T, KC_S, KC_G
 #define ALTERN_L3 KC_Q, KC_X, KC_M, KC_C, KC_V
 
-#define ALTERN_R1 KC_J, KC_K, KC_O, KC_U, KC_COMM
-#define ALTERN_R2 KC_Y, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_P, KC_F, KC_QUOT, KC_MINS, KC_DOT
+#define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_J
+#define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
+#define ALTERN_R3 KC_P, KC_F, KC_DOT, KC_MINS, KC_COMM
 
 #endif
 
