75ac86fecfc2bb52e0635fc7b6ec6a1033fca872
Kristoffer Grönlund <k@ziran.se>
2023-12-05 08:43:42 +0100

    swap z and q

--- users/krig/layout/krigite.h
+++ users/krig/layout/krigite.h
@@ -3,15 +3,15 @@
 //
 // slight mod of graphite
 // by rdavison
-//   b l d w z  ' y o u j
+//   b l d w q  ' y o u j
 //   n r t s g  k h a e i
-//   q x m c v  p f . - ,
+//   z x m c v  p f . - ,
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_B, KC_L, KC_D, KC_W, KC_Z
+#define ALTERN_L1 KC_B, KC_L, KC_D, KC_W, KC_Q
 #define ALTERN_L2 KC_N, KC_R, KC_T, KC_S, KC_G
-#define ALTERN_L3 KC_Q, KC_X, KC_M, KC_C, KC_V
+#define ALTERN_L3 KC_Z, KC_X, KC_M, KC_C, KC_V
 
 #define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_J
 #define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
