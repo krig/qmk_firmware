28964724bbf4e2d5c67d4b67788eaa8c189fe9fd
Kristoffer Grönlund <k@ziran.se>
2023-12-07 15:41:18 +0100

    never stop tweaking

--- users/krig/layout/krigite.h
+++ users/krig/layout/krigite.h
@@ -3,19 +3,19 @@
 //
 // slight mod of graphite
 // by rdavison
-//   b l d w q  ' y o u j
-//   n r t s g  k h a e i
-//   z x m c v  p f . - ,
+//   b l d w z  ' y o u j
+//   n r t s g  p h a e i
+//   x q m c v  k f . - ,
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_B, KC_L, KC_D, KC_W, KC_Q
+#define ALTERN_L1 KC_B, KC_L, KC_D, KC_W, KC_Z
 #define ALTERN_L2 KC_N, KC_R, KC_T, KC_S, KC_G
-#define ALTERN_L3 KC_Z, KC_X, KC_M, KC_C, KC_V
+#define ALTERN_L3 KC_X, KC_Q, KC_M, KC_C, KC_V
 
 #define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_J
-#define ALTERN_R2 KC_K, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_P, KC_F, KC_DOT, KC_MINS, KC_COMM
+#define ALTERN_R2 KC_P, KC_H, KC_A, KC_E, KC_I
+#define ALTERN_R3 KC_K, KC_F, KC_DOT, KC_MINS, KC_COMM
 
 #endif
 
